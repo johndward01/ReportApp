3e310229b90412d41aeac74ec8eacfab69cad155
johndward01 <johndward01@gmail.com>
2022-10-21 17:03:54 -0500

    Add project files.

---
+++ ReportApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+var path = @"C:\Users\johnd\Desktop\dummy data\SampleFile.txt";
+
+var file = File.ReadAllLines(path);
+
+var record = file[1].Take(4);
+
+foreach (var letter in record)
+{
+    Console.Write($"{letter}");
+}
---
+++ ReportApp/Validate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportApp;
+internal class Validate
+{
+    public bool RecordNumber_IsValid(string record)
+    {
+        if (record.Length != 4)
+        {
+            return false;
+        }
+
+        throw new NotImplementedException();
+
+    }
+}
